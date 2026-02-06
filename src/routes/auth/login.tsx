e1a09f4ddcd2d44584340ec91a93725d72f550b4
sbstnrmrz <ramicastasebas@gmail.com>
2026-02-06 12:48:05 -0400

    init

---
+++ src/routes/auth/login.tsx
@@ -0,0 +1,15 @@
+import { createFileRoute } from "@tanstack/react-router"
+
+export const Route = createFileRoute('/auth/login')({
+  component: Login,
+})
+
+function Login() {
+  return (
+    <div>
+      Login
+
+    </div>
+  )
+}
+
